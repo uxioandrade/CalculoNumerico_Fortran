782f318dcc90489a2e9d04cc7e6affbaa8e156c3
Uxío García Andrade <uxiog21@gmail.com>
2018-11-09 16:48:45 +0100

    dicotomia working

--- Practica1/dicoto.f95
+++ Practica1/dicoto.f95
@@ -1,8 +1,15 @@
 subroutine dicoto(f3,x0, c, eps, delta, nitmax, a, b)
 
-  real(4), external :: ../FUNCTIONS/f3.f95
+  real(4), external :: f3
   real(4) :: x0, c, eps, delta, a, b, aux
   integer :: i, nitmax
+
+  !Si f(a) > 0 y f(b) < 0, entonces cambiamos a y b
+
+  open(unit = 2, file = 'dicoto.sal', status = 'old',action = 'write')
+
+  nitmax = ceiling((log(b-a) - log(eps))/log(2.0)-1)
+  write(*,*) nitmax
 
   !Si f(a) > 0 y f(b) < 0, entonces cambiamos a y b
   if(f3(a) > 0) then
@@ -11,15 +18,11 @@ subroutine dicoto(f3,x0, c, eps, delta, nitmax, a, b)
     b = aux
   end if
 
-  open(unit = 2)
-
-  nitmax = ceil(log((b-a)/eps)/log(2)-1)
-
-  write(2,*) "           n     ", "   Xn  " , "       |F(c)| ", "       |c - delta|  "
+  write(2,*) "          n     ", "   a     " , "         b       ", "       |F(c)|   ", "        |c - delta|  "
 
   do i= 1,nitmax
     c = (a+b)/2
-    write(2,*) i, xn, ABS(c), ABS(f3(c))
+    write(2,*) i, a, b, ABS(c), ABS(f3(c))
     if(ABS(f3(c)) < delta) then
       write(*,*) "Construíronse",i,"elementos, sendo o último:",f3(c),"Test de parada: distancia ao límite"
         exit
@@ -35,8 +38,8 @@ subroutine dicoto(f3,x0, c, eps, delta, nitmax, a, b)
     if(f3(c) > 0) then
       b = c
     end if
-end do
+    end do
 
-close(2,status = 'keep')
+    close(2,status = 'keep')
 
-end subroutine
+end subroutine dicoto
--- Practica1/prin_dicoto.f95
+++ Practica1/prin_dicoto.f95
@@ -1,9 +1,11 @@
 program prin_dicoto
 
   implicit none
-  real(4),external :: ../FUNCTIONS/f3.f95
+  real(4),external :: f3
   real(4) :: x0,c,eps,delta,a,b
   integer :: nitmax
 
   call lecdic(x0, eps, delta, a,b)
   call dicoto(f3,x0, c, eps, delta, nitmax,a,b)
+
+end program prin_dicoto
